f1298aea6a8a389629ea7a19a702b5183a3b274a
Raj Khare <seven.triangles@icloud.com>
2019-04-08 23:50:26 +0530

    Change ALLOWED_HOSTS

--- reminder/settings.py
+++ reminder/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['reminder--app.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
